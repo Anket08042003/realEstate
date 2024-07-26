b2ffa6270d5d4120f7c656c0a0eaff20572ecb2a
Anket08042003 <anketkadam61@gmail.com>
2024-07-26 23:44:44 +0530

    Data is getting fetch from Google OAuth

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='font-bold text-sm bg-slate-200 shadow-md'>
         <div className='flex flex-row justify-between items-center max-w-6xl mx-auto p-3'>
@@ -21,7 +23,10 @@ function Header() {
           <Link to={'/'}>
           <li className='hidden sm:inline text-slate-700'>Home</li></Link>
           <Link to={'/about'}><li className='hidden sm:inline text-slate-700'>About</li></Link>
-          <Link to={'/sign-in'}><li>SignIn</li></Link>
+          
+          <Link to={'/sign-in'}>
+          {currentUser ? (<img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'/>):<li>SignIn</li>}
+          </Link>
         </ul>
         </div>
     </header>
