29b17b2a890ff58ca5747617662ca2b9f348af99
Anket08042003 <anketkadam61@gmail.com>
2024-08-03 23:50:32 +0530

    Deletion of listing (client and server side) done

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUserListings, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const userRouter= express.Router();
